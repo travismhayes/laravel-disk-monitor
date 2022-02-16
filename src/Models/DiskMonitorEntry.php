9f1cbe7d4270501a835c38f52645038ca54ddf28
Travis <travis@travismhayes.com>
2022-02-16 08:27:47 -0800

    add test for multiple disk support

--- src/Models/DiskMonitorEntry.php
+++ src/Models/DiskMonitorEntry.php
@@ -11,6 +11,10 @@ class DiskMonitorEntry extends Model
 
     public $guarded = [];
 
+    protected $casts = [
+        'file_count' => 'integer'
+    ];
+
     public static function last(): ?self
     {
         return static::orderByDesc('id')->first();
--- tests/Feature/Commands/RecordDiskMetricsCommandTest.php
+++ tests/Feature/Commands/RecordDiskMetricsCommandTest.php
@@ -24,7 +24,7 @@ class RecordDiskMetricsCommandTest extends TestCase
         $entry = DiskMonitorEntry::last();
         $this->assertEquals(0, $entry->file_count);
 
-        Storage::put('test.txt', 'test');
+        Storage::disk('local')->put('test.txt', 'test');
         $this->artisan(RecordDiskMetricsCommand::class)->assertExitCode(0);
         $entry = DiskMonitorEntry::last();
         $this->assertEquals(1, $entry->file_count);
@@ -34,6 +34,7 @@ class RecordDiskMetricsCommandTest extends TestCase
     public function it_will_record_the_file_count_for_multiple_disks()
     {
         config()->set('disk-monitor.disk_names', ['local', 'anotherDisk']);
+        Storage::disk('anotherDisk')->put('test.txt', 'test');
 
         $this->artisan(RecordDiskMetricsCommand::class)->assertExitCode(0);
 
@@ -41,7 +42,11 @@ class RecordDiskMetricsCommandTest extends TestCase
         $this->assertCount(2, $entries);
 
         $this->assertEquals('local', $entries[0]->disk_name);
+        $this->assertEquals(0, $entries[0]->file_count);
+
         $this->assertEquals('anotherDisk', $entries[1]->disk_name);
+        $this->assertEquals(1, $entries[1]->file_count);
+
     }
 
 }
